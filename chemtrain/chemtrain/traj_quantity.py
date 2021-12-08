b4de1f307e167fbdac9784bed2a28a730d8eb7a0
stephan <stephan.thaler@tum.de>
2021-12-08 17:39:30 +0100

    Added isothermal compressibiliy function and improved documentation

--- chemtrain/chemtrain/traj_quantity.py
+++ chemtrain/chemtrain/traj_quantity.py
@@ -3,12 +3,31 @@ single snapshots.
 
 Builds on the TrajectoryState object defined in traj_util.py.
 """
-from jax import vmap
+from jax import vmap, numpy as jnp
 from jax_md import simulate, quantity
 
 
 def volumes(traj_state):
-    """Returns array of volumes for all boxes in a NPT trajectory."""
+    """Returns array of volumes for all boxes in a NPT trajectory.
+
+    Args:
+        traj_state: TrajectoryState containing the NPT trajectory
+    """
     dim = traj_state.sim_state[0].position.shape[-1]
     boxes = vmap(simulate.npt_box)(traj_state.trajectory)
     return vmap(quantity.volume, (None, 0))(dim, boxes)
+
+
+def isothermal_compressibility(traj_state, kbt):
+    """Returns isothermal compressibility of a system in the NPT ensemble
+    via the fluctuation formula.
+
+    Args:
+        traj_state: TrajectoryState containing the NPT trajectory
+        kbt: Temperature * Boltzmann constant
+    """
+    volume_traj = volumes(traj_state)
+    mean_volume = jnp.mean(volume_traj, axis=0)
+    mean_sq_volume = jnp.mean(volume_traj**2, axis=0)
+    kappa = (mean_sq_volume - mean_volume**2) / (mean_volume * kbt)
+    return kappa
