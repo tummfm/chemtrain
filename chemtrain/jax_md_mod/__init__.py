0759b249c264ee9db1df9bfe4011a5928a72f054
Paul <paul.fuchs@tum.de>
2024-04-19 07:47:21 +0200

    Fixed bug of neighbor list error not printable

--- chemtrain/jax_md_mod/__init__.py
+++ chemtrain/jax_md_mod/__init__.py
@@ -1,31 +1,9 @@
-# Copyright 2023 Multiscale Modeling of Fluid Materials, TU Munich
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import sys
 
 from jax import Array
 import jax.numpy as jnp
 import jax_md.partition
-
-from . import (
-  custom_energy,
-  custom_interpolate,
-  custom_quantity,
-  custom_simulator,
-  custom_space,
-  io
-)
+from jax_md import dataclasses
 
 def uncache(exclude):
   """Remove package modules from cache except excluded ones.
@@ -65,5 +43,48 @@ def is_box_valid(box: Array) -> bool:
     return jnp.bool_(jnp.all(jnp.triu(box) == box))
   return False
 
+@dataclasses.dataclass
+class PartitionError:
+  """A struct containing error codes while building / updating neighbor lists.
+
+  Attributes:
+    code: An array storing the error code. See `PartitionErrorCode` for
+      details.
+  """
+  code: Array
+
+  def update(self, bit: bytes, pred: Array) -> Array:
+    """Possibly adds an error based on a predicate."""
+    zero = jnp.zeros((), jnp.uint8)
+    bit = jnp.array(bit, dtype=jnp.uint8)
+    return PartitionError(self.code | jnp.where(pred, bit, zero))
+
+  def __str__(self) -> str:
+    """Produces a string representation of the error code."""
+
+    try:
+      if not jnp.any(self.code):
+        return ''
+    except Exception as err:
+      return f'Error code not available ({err})'
+
+    if jnp.any(self.code & jax_md.partition.PEC.NEIGHBOR_LIST_OVERFLOW):
+      return 'Partition Error: Neighbor list buffer overflow.'
+
+    if jnp.any(self.code & jax_md.partition.PEC.CELL_LIST_OVERFLOW):
+      return 'Partition Error: Cell list buffer overflow'
+
+    if jnp.any(self.code & jax_md.partition.PEC.CELL_SIZE_TOO_SMALL):
+      return 'Partition Error: Cell size too small'
+
+    if jnp.any(self.code & jax_md.partition.PEC.MALFORMED_BOX):
+      return ('Partition Error: Incorrect box format. Expecting upper '
+              'triangular.')
+
+    raise ValueError(f'Unexpected Error Code {self.code}.')
+
+  __repr__ = __str__
+
 jax_md.partition.is_box_valid = is_box_valid
+jax_md.partition.PartitionError = PartitionError
 uncache('jax_md.partition')
